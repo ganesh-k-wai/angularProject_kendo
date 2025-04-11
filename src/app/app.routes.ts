cd3b154b7a669a24eb46746d5579a8ed70183bdf
ganesh-k-wai <ganesh.kadam@waiin.com>
2025-04-11 20:35:49 +0530

    added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LeadMgtComponent } from './lead-mgt/lead-mgt.component';
 
-export const routes: Routes = [{ path: '', component: HomeComponent }];
+export const routes: Routes = [
+  { path: '', component: LeadMgtComponent }, // Redirect to home component
+];
---
+++ src/app/lead-mgt/lead-mgt.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { HomeComponent } from '../home/home.component';
+
+@Component({
+  selector: 'app-lead-mgt',
+  imports: [HomeComponent],
+  templateUrl: './lead-mgt.component.html',
+  styleUrl: './lead-mgt.component.css',
+})
+export class LeadMgtComponent {}
