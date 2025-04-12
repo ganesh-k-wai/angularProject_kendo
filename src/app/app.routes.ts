344ed8403cd5795675fcb6e3f893f5a712c68904
ganesh-k-wai <ganesh.kadam@waiin.com>
2025-04-12 18:25:12 +0530

    all done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,13 @@ import { HomeComponent } from './home/home.component';
 import { LeadMgtComponent } from './lead-mgt/lead-mgt.component';
 
 export const routes: Routes = [
-  { path: '', component: LeadMgtComponent }, // Redirect to home component
+  { path: '', redirectTo: '/lead-management', pathMatch: 'full' },
+  { path: 'dashboard', component: HomeComponent },
+  { path: 'agent-management', component: HomeComponent },
+  { path: 'calendar', component: HomeComponent },
+  { path: 'ez-quote', component: HomeComponent },
+  { path: 'lead-management', component: LeadMgtComponent },
+  { path: 'activities', component: HomeComponent },
+  { path: 'workflow', component: HomeComponent },
+  { path: 'chats', component: HomeComponent },
 ];
--- src/app/lead-mgt/lead-mgt.component.ts
+++ src/app/lead-mgt/lead-mgt.component.ts
@@ -1,10 +1,116 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { KENDO_CHARTS } from '@progress/kendo-angular-charts';
 import { HomeComponent } from '../home/home.component';
 
+import {
+  DataBindingDirective,
+  KENDO_GRID,
+  KENDO_GRID_EXCEL_EXPORT,
+  KENDO_GRID_PDF_EXPORT,
+} from '@progress/kendo-angular-grid';
+import { KENDO_INPUTS } from '@progress/kendo-angular-inputs';
+import { process } from '@progress/kendo-data-query';
+import { SVGIcon, fileExcelIcon, filePdfIcon } from '@progress/kendo-svg-icons';
+import { employees } from './employees';
+import { images } from './images';
+import { FormsModule } from '@angular/forms';
+
+export interface Employee {
+  id: string;
+  full_name: string;
+  job_title: string;
+  country: string;
+  is_online: boolean;
+  rating: number;
+  target: number;
+  budget: number;
+  phone: string;
+  address: string;
+  img_id: string;
+  gender: string;
+}
 @Component({
   selector: 'app-lead-mgt',
-  imports: [HomeComponent],
+  standalone: true,
   templateUrl: './lead-mgt.component.html',
   styleUrl: './lead-mgt.component.css',
+  imports: [
+    CommonModule,
+    KENDO_GRID,
+    KENDO_CHARTS,
+    KENDO_INPUTS,
+    KENDO_GRID_PDF_EXPORT,
+    KENDO_GRID_EXCEL_EXPORT,
+    HomeComponent,
+    FormsModule,
+  ],
 })
-export class LeadMgtComponent {}
+export class LeadMgtComponent implements OnInit {
+  @ViewChild(DataBindingDirective) dataBinding!: DataBindingDirective;
+  public gridData: Employee[] = employees;
+  public gridView: Employee[] = [];
+  public mySelection: string[] = [];
+  public pdfSVG: SVGIcon = filePdfIcon;
+  public excelSVG: SVGIcon = fileExcelIcon;
+  public large: string = 'large'; // Define the 'large' property
+
+  public ngOnInit(): void {
+    this.gridView = this.gridData;
+  }
+
+  public onFilter(value: Event): void {
+    if (!this.gridData || this.gridData.length === 0) {
+      return;
+    }
+
+    this.gridView = process(this.gridData, {
+      filter: {
+        logic: 'or',
+        filters: [
+          {
+            field: 'full_name',
+            operator: 'contains',
+            value,
+          },
+          {
+            field: 'job_title',
+            operator: 'contains',
+            value,
+          },
+          {
+            field: 'budget',
+            operator: 'contains',
+            value,
+          },
+          {
+            field: 'phone',
+            operator: 'contains',
+            value,
+          },
+          {
+            field: 'address',
+            operator: 'contains',
+            value,
+          },
+        ],
+      },
+    }).data;
+
+    this.dataBinding.skip = 0;
+  }
+
+  public photoURL(dataItem: { img_id: string; gender: string }): string {
+    const code: string = dataItem.img_id + dataItem.gender;
+    const image: { [Key: string]: string } = images;
+
+    return image[code];
+  }
+
+  public flagURL(dataItem: { country: string }): string {
+    const code: string = dataItem.country;
+    const image: { [Key: string]: string } = images;
+
+    return image[code];
+  }
+}
